7692a9ca9ddf994fb5d562c4e7bf6f68e2838c57
misterche51 <alexanderchernykh88@gmail.com>
2019-03-08 20:47:41 +0300

    js2
    javascript для map

--- js/device.js
+++ js/device.js
@@ -1,35 +1,34 @@
-	var moreInfo = document.querySelector(".about__btn--popup");
-	var mapZoom = document.querySelector(".about__map");
-	var mapPopup = document.querySelector("#modal-map");
-  var mapExit = mapPopup.querySelector(".modal__btn-exit");
-	var popup = document.querySelector(".modal__feedback");
-	var buttonExit = document.querySelector(".modal__btn-exit");
-	var form = popup.querySelector("form");
-	var feedbackUsername = popup.querySelector(".form__field--username");
-	var feedbackUsermail = popup.querySelector(".form__field--usermail");
-	var feedbackUsermessage = popup.querySelector(".form__field--usermessage");
-	var isStorageSupport = true;
-  var storage = {
+var writeUs = document.querySelector(".about__btn--popup");
+var mapHtml = document.querySelector(".about__map");
+var mapModal = document.querySelector("#modal-map");
+var mapExit = mapModal.querySelector(".modal__btn-exit");
+var popup = document.querySelector(".modal__feedback");
+var buttonExit = document.querySelector(".modal__btn-exit");
+var form = popup.querySelector("form");
+var feedbackUsername = popup.querySelector(".form__field--username");
+var feedbackUsermail = popup.querySelector(".form__field--usermail");
+var feedbackUsermessage = popup.querySelector(".form__field--usermessage");
+var isStorageSupport = true;
+var storage = {
       name: "",
       email: ""
-  };
+};
 
 
-  try {
-    storage.name = localStorage.getItem("feedbackUsername");
-    storage.email = localStorage.getItem("feedbackUsermail");
-    } catch (err) {
-      isStorageSupport = false;
-    }
+try {
+  storage.name = localStorage.getItem("feedbackUsername");
+  storage.email = localStorage.getItem("feedbackUsermail");
+} catch (err) {
+    isStorageSupport = false;
+  }
  	
+writeUs.addEventListener("click", onPopupShow);
+buttonExit.addEventListener("click", onPopupClose);
 
-  moreInfo.addEventListener("click", onPopupShow);
-  buttonExit.addEventListener("click", onPopupClose);
 
-
-  function onPopupShow (evt) {
+function onPopupShow (evt) {
     evt.preventDefault();
-     popup.classList.add("modal-show");
+    popup.classList.add("modal--show");
     if (isStorageSupport) {
        storage.name = localStorage.getItem("feedbackUsername");
        storage.email = localStorage.getItem("feedbackUsermail");
@@ -51,95 +50,106 @@
     }
    
     window.addEventListener("keydown", onPopupEscapePress);
-  }
+};
 
-  function onPopupClose (evt) {
-   evt.preventDefault();
-    popup.classList.remove("modal-show","modal-shake");
+function onPopupClose (evt) { 
+    evt.preventDefault();
+
+    popup.classList.remove("modal--show","modal--error");
     if (isStorageSupport) {
        localStorage.setItem("feedbackUsername", feedbackUsername.value);
        localStorage.setItem("feedbackUsermail", feedbackUsermail.value);
     }
-    feedbackUsername.classList.remove("field-invalided");
-    feedbackUsermail.classList.remove("field-invalided");
-    feedbackUsermessage.classList.remove("field-invalided");
+    feedbackUsername.classList.remove("field-invalid");
+    feedbackUsermail.classList.remove("field-invalid");
+    feedbackUsermessage.classList.remove("field-invalid");
  }
 
-  function onPopupEscapePress (evt) {
+function onPopupEscapePress (evt) {
     if (evt.keyCode === 27) {
-      evt.preventDefault();
-      onPopupClose();
+      onPopupClose(evt);
       window.removeEventListener("keydown", onPopupEscapePress);
     }
-  }
+}
 
 
+form.addEventListener("submit", onFormSubmit);
+feedbackUsername.addEventListener("focus", onFieldFocus);
+feedbackUsermail.addEventListener("focus", onFieldFocus);
+feedbackUsermessage.addEventListener("focus", onFieldFocus);
 
-   mapZoom.addEventListener("click", function (evt) {
-  	evt.preventDefault();
-  	mapPopup.classList.add("modal-show");
-  });
+function onFormSubmit (evt) {
+  var isFormValid = true;
+  checkElementEmpty(feedbackUsername);
+  checkElementEmpty(feedbackUsermail);
+  checkElementEmpty(feedbackUsermessage);
 
-   window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+    if (!isFormValid) {
       evt.preventDefault();
-      if (mapPopup.classList.contains("modal-show")) {
-          mapPopup.classList.remove("modal-show");
-      } 
-    };
-  });
+      popup.classList.remove("modal--error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal--error");
+    } 
+      function checkElementEmpty (elem) {
+        if (!elem.value) {
+          isFormValid = false;
+          elem.classList.add("field-invalid");
+        }
+      }
+}
 
-  mapExit.addEventListener("click", function (evt) {
-    evt.preventDefault();
-     mapPopup.classList.remove("modal-show");
+function onFieldFocus (evt) {
+    evt.target.classList.remove("field-invalid");
+    }
+
+
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map("modal-map", {
+            center: [55.686980, 37.529654],
+            zoom: 14,
+            controls: ["zoomControl","searchControl"]
+    });
+
+     var placemark = new ymaps.Placemark(myMap.getCenter(), {
+        balloonContentHeader: 'Device на ул.Строителей, 15',
+        balloonContentBody: '<img src="img/decorative/logo-device-footer.svg" width="163" height="35"> <br/> ' +
+            '<a href="tel:+7-495-495-95-95">+7(495)495-95-95</a><br/>' +
+            '<b>Режим работы</b> <br/> С 10-00 до 20-00.',
+        balloonContentFooter: 'Информация предоставлена:<br/>OOO "Девайс"',
+        hintContent: 'Магазин Device находится здесь'
+    });
+    // Добавим метку на карту.
+    myMap.geoObjects.add(placemark);
+    // Откроем балун на метке.
+    placemark.balloon.open();
 });
 
 
 
 
+  mapHtml.addEventListener("click", onMapShow);
+  mapExit.addEventListener("click", onMapClose);
+  
+  function onMapShow (evt) {
+    evt.preventDefault();
+    mapModal.classList.add("modal--show");
+    window.addEventListener("keydown", onMapEscapePress);
+  }
 
-
-
-
-
-
-    form.addEventListener("submit", onFormSubmit);
-    feedbackUsername.addEventListener("focus", onFieldFocus);
-    feedbackUsermail.addEventListener("focus", onFieldFocus);
-    feedbackUsermessage.addEventListener("focus", onFieldFocus);
-
-    function onFormSubmit (evt) {
-      var isFormValid = true;
-
-         if (!feedbackUsername.value) {
-          isFormValid = false;
-          feedbackUsername.classList.remove("field-invalided");
-          feedbackUsername.offsetWidth = feedbackUsername.offsetWidth;
-          feedbackUsername.classList.add("field-invalided");
-         }
-
-          if (!feedbackUsermail.value) {
-          isFormValid = false;
-          feedbackUsermail.classList.remove("field-invalided");
-          feedbackUsermail.offsetWidth = feedbackUsermail.offsetWidth;
-          feedbackUsermail.classList.add("field-invalided");
-         }
-
-          if (!feedbackUsermessage.value) {
-          isFormValid = false;
-          feedbackUsermessage.classList.remove("field-invalided");
-          feedbackUsermessage.offsetWidth = feedbackUsermessage.offsetWidth;
-          feedbackUsermessage.classList.add("field-invalided");
-        }
-
-          if (!isFormValid) {
-            evt.preventDefault();
-            popup.classList.remove("modal-shake");
-            popup.offsetWidth = popup.offsetWidth;
-            popup.classList.add("modal-shake");
-            } 
+ function onMapEscapePress (evt) {
+    if (evt.keyCode === 27) {
+      onMapClose(evt);
+      window.removeEventListener("keydown", onMapEscapePress);
     }
+  }
+  
+  function onMapClose (evt) { 
+    evt.preventDefault();
 
-    function onFieldFocus (evt) {
-      evt.target.classList.remove("field-invalided");
-    }
+    mapModal.classList.remove("modal--show");
+  }
+
+
+
+
